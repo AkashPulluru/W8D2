d9ccc8919c87acba52f535cf07010cfbca37e937
Harrison Liang <liangharrison123@gmail.com>
2023-10-10 14:24:38 -0400

    unique arr

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,12 @@
 // Your code here
+Array.prototype.uniq = function (arr) {
+    const uniqueArray = [];
+
+    this.forEach(item => {
+        if (!uniqueArray.includes(item)){
+            uniqueArray.push(item);
+        }
+    })
+
+    return uniqueArray;
+}
