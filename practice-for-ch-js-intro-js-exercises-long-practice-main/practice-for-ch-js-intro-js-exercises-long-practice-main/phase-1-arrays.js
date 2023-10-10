8e4ba8b5478f369241a8fa8bc83c30a9470b411a
Harrison Liang <liangharrison123@gmail.com>
2023-10-10 14:50:43 -0400

    two sum

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,5 +1,5 @@
 // Your code here
-Array.prototype.uniq = function (arr) {
+Array.prototype.uniq = function () {
     const uniqueArray = [];
 
     this.forEach(item => {
@@ -10,3 +10,22 @@ Array.prototype.uniq = function (arr) {
 
     return uniqueArray;
 }
+
+// console.log([1,2,2,3,3,3].uniq())
+
+Array.prototype.twoSum = function(){
+    const twoSumArr = [];
+
+    this.forEach((num, index) => {
+        this.forEach((num2, index2) => {
+            if (num + num2 === 0 && index < index2) {
+                twoSumArr.push([index, index2]);
+            }
+        })
+    })
+
+    return twoSumArr;
+}
+
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
